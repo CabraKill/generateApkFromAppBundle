59bf5150ec1bc743d9c8f82fe9facd6c96650626
Thiago Felipe <thiago.felipe@grupofasitec.com.br>
2021-01-22 14:07:43 -0300

    first commit

---
+++ generate_apk.py
@@ -0,0 +1,43 @@
+# Python script para copiar o bundletool para a pasta de build do appbundle e criar os apks
+# Programmer: Raphael
+
+import os
+import tkinter as tk
+from tkinter import filedialog
+import zipfile
+
+
+def main():
+    os.system("color b")
+    print("Choose the base flutter project directory\n\n")
+    root = tk.Tk()
+    root.withdraw()
+
+    FILE_PATH = filedialog.askdirectory()
+    if(not FILE_PATH):
+        print("Directory not found")
+        return
+    SUB_FOLDER = "/build/app/outputs/bundle/release"
+    DESTINATION = FILE_PATH + SUB_FOLDER
+
+    print("destination: " + DESTINATION)
+    # os.system("copy bundletool.jar \"" + FILE_PATH +
+    #           SUB_FOLDER + "bundletool.jar\" /Y")
+
+    os.system(
+        f"java -jar bundletool.jar build-apks --bundle=\"{FILE_PATH}{SUB_FOLDER}/app.aab\" --output=\"{FILE_PATH}{SUB_FOLDER}/app.apks\" --mode=universal")
+
+    def apk_path(file):
+        apks = DESTINATION + "/" + file
+        print("APKS location: "+apks)
+        return apks
+    with zipfile.ZipFile(apk_path("app.apks"), 'r') as zip_ref:
+        zip_ref.extractall(apk_path(""))
+        os.system("dir")
+    os.chdir(FILE_PATH+SUB_FOLDER)
+    os.system(f"start .")
+    input("\n\n...........Press to exit...........")
+
+
+if __name__ == "__main__":
+    main()
